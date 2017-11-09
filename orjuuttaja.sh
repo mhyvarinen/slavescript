670c8f8266d8fe4d139852fc35256cb5509072a1
mhyvarinen <31437091+mhyvarinen@users.noreply.github.com>
2017-11-09 13:24:10 +0000

    Update orjuuttaja.sh

--- orjuuttaja.sh
+++ orjuuttaja.sh
@@ -2,18 +2,18 @@ sudo apt-get update
 
 sudo apt-get install -y puppet git
 
-git clone https://github.com/TatuE/puppetslaver.git
+git clone https://github.com/mhyvarinen/orjuuttaja.git
 
-sudo cp -r puppetslaver /etc/puppet/modules/
+sudo cp -r orjuuttaja /etc/puppet/modules/
 
 cd /etc/puppet/
 
-sudo puppet apply --modulepath modules/ -e 'class {"puppetslaver":}'
+sudo puppet apply --modulepath modules/ -e 'class {"orjuuttaja":}'
 
 sudo puppet agent --enable
 
 sudo service puppet restart
 
 echo " "
-echo "Slaving complete!"
+echo "Orjuutus valmis"
 echo " "
